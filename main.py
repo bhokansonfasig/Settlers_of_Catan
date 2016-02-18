db44ac5d47568cc4122bb773d3cf13c8103e28e6
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-02-17 18:57:01 -0600

    Changed names of files to avoid possible confusion with preset modules

---
+++ main.py
@@ -0,0 +1,12 @@
+# Settlers of Catan
+# Version 0.0.1
+# Created by Ben Hokanson-Fasig, Aman Abhishek, and Alex Scherer?
+# Last update 02-17-16
+
+version = "0.0.1"
+
+
+from catan_graphics import *
+from catan_logic import *
+
+#if __name__ == '__main__':
