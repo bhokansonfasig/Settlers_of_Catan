ad70718d7ec3e00678b54dbc548dbcf2a24f262f
Aman Abhishek <aabhishek@wisc.edu>
2016-02-24 10:17:59 +0530

    saw no utility of road and point classes in the long run and changed them to functions.

--- construction.py
+++ construction.py
@@ -3,6 +3,11 @@ import sys
 edge_tiles =    [0,1,2,3,4,5,6,13,20,27,34,41,48,47,46,45,44,43,42,35,28,21,14,7]
 hidden_tiles =  [0,1,2,3,4,5,6,13,20,27,34,41,48,47,46,45,44,43,42,35,28,21,14,7,8,12,13,29,36,40]
 
+#develope a list of already constructed objects on the board
+constructed_roads = []
+constructed_settlements = []
+constructed_cities = []
+
 #checks if two given tiles are neighbors, doesn't matter if a road cannot be constructed between them (like 40 and 41)
 def neighbor_tile(hex1,hex2): #hex1 must be smaller than hex2
 	
@@ -45,49 +50,47 @@ def neighbor_tile(hex1,hex2): #hex1 must be smaller than hex2
 		print("What the hell")
 		sys.exit()
 
+#checks the validity of a point	
+def point(hex1,hex2,hex3):
+	if((hex1==hex2) or (hex2==hex3) or (hex3==hex1)):
+		print("Enter three unique tiles.")
+		return False
 
+	# sort x,y,z in increasing order.
+	x = min(hex1,hex2,hex3)
+	z = max(hex1,hex2,hex3)
+	y = hex1+hex2+hex3-x-z
 	
-class point:
-	
-	def __init__(self,hex1,hex2,hex3):
-		if((hex1==hex2) or (hex2==hex3) or (hex3==hex1)):
-			print("Enter three unique tiles.")
-			sys.exit()
-
-		# sort x,y,z in increasing order.
-		x = min(hex1,hex2,hex3)
-		z = max(hex1,hex2,hex3)
-		y = hex1+hex2+hex3-x-z
-		
-		#check if such a point exists
-		# print(neighbor_tile(x,y), neighbor_tile(y,z), neighbor_tile(x,z))
-		if(neighbor_tile(x,y) and neighbor_tile(y,z) and neighbor_tile(x,z)):
-			if((x in hidden_tiles) and (y in hidden_tiles) and (z in hidden_tiles)): #like 40,47,48
-				print("Point lies in the sea!")
-				sys.exit()
-			else:
-				pass
+	#check if such a point exists
+	# print(neighbor_tile(x,y), neighbor_tile(y,z), neighbor_tile(x,z))
+	if(neighbor_tile(x,y) and neighbor_tile(y,z) and neighbor_tile(x,z)):
+		if((x in hidden_tiles) and (y in hidden_tiles) and (z in hidden_tiles)): #like 40,47,48
+			print("Point lies in the sea!")
+			return False
 		else:
-			print("Invalid point.")
-			sys.exit()
-		
-class road:
+			return True
+	else:
+		print("Invalid point.")
+		return False
 
-	def __init__(self,hex1,hex2):
-		x = min(hex1,hex2)
-		y = max(hex1,hex2)
-		both_hidden = (x in hidden_tiles) and (y in hidden_tiles) #like 13 and 12 cant have a road bw them, 11 and 12 is fine
-		if(neighbor_tile(x,y) and (not both_hidden)):
-			pass
-		else:
-			print("Invalid road.")
-			sys.exit()
+#checks the possibility of road construction ignoring if there is already a road there
+def road(hex1,hex2):
+	x = min(hex1,hex2)
+	y = max(hex1,hex2)
+	both_hidden = (x in hidden_tiles) and (y in hidden_tiles) #like 13 and 12 cant have a road bw them, 11 and 12 is fine
+	if(neighbor_tile(x,y) and (not both_hidden)):
+		return True
+	else:
+		print("Invalid road.")
+		return False
 
-while(True):
+x = 17
+y = 23
+z = 24
+while(road(x,y)):
 	x = int(input("hex1: "))
 	y = int(input("hex2: "))
 	# z = int(input("hex3: "))
-	a = road(x,y)
 	# if(neighbor_tile(x,y)):
 	# 	print("They are neighbors.\n")
 	# else:
