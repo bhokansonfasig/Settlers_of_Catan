a673a47bec3ce9db9e2e903c8bd27c3b2abb7909
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-02-24 14:07:28 -0600

    Tweaked player class

--- player.py
+++ player.py
@@ -1,34 +1,39 @@
-class player:
-	
+class Player:
+
 	def __init__(self,index):
-		#Index determines the turn order
+		# Index of player. Set despite turn order
+		#  (i.e. player with index 1 may play third)
 		self.index = index
-	
+
 		# Building resources limit
 		self.road_max = 15
 		self.settlement_max = 5
 		self.city_max = 4
-	
-		# Acquired resources 
+
+		# Structures built
+		self.roads = []
+		self.settlements = []
+		self.cities = []
+
+		# Acquired resources
 		self.wood = 0
 		self.brick = 0
 		self.wheat = 0
 		self.sheep = 0
 		self.stone = 0
-	
+
 		#Development cards: lets leave these for now
-	
+
 		# Score keeping
 		self.road_length = 0 #we better deal with this from early on
 		self.score = 0
 
-	def resource_cards_count(self):
-		return self.wood + self.brick + self.wheat + self.sheep + self.stone	
+	def resource_count(self):
+		return self.wood + self.brick + self.wheat + self.sheep + self.stone
 
-	#counting resource cards when the robber comes
-	def rob(self):
-		if(self.resource_cards_count() > 7): 
+	# Counting resource cards when the robber comes
+	def robbable(self):
+		if(self.resource_count() > 7):
 			return True
-		else: 
+		else:
 			return False
-
