615eea31c13198af5464f220c49fb38aa687745d
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-02-26 14:33:38 -0600

    Forgot to remove claim function definitions last time. They're gone now.

--- main.py
+++ main.py
@@ -21,9 +21,8 @@ def new_game(splash,board):
     from random import shuffle
     from player import Player
     from tiles import Tile
-    from catan_logic import set_tiles, claim_settlement, claim_road
-    from catan_logic import give_resource, point_resources, check_winner
-    from catan_logic import roll_dice, distribute_resources
+    from catan_logic import set_tiles, roll_dice, check_winner
+    from catan_logic import give_resource, point_resources, distribute_resources
     from catan_graphics import open_board_window, close_board_window, close_all
     from catan_graphics import set_players, get_tiles, draw_tiles, draw_stats
     from catan_graphics import player_place_settlement, player_place_road
