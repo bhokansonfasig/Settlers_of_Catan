0ee77514d62593ea25e44b305cf189b8b19cd9b4
Aman Abhishek <aabhishek@wisc.edu>
2016-02-26 16:36:36 -0600

    added equality operator

--- road.py
+++ road.py
@@ -2,6 +2,9 @@ class Road:
     def __init__(self, point1, point2):
         from point import Point
         self.coordinates = sorted([point1.coordinate,point2.coordinate])
+        # print(self.coordinates)
+        print("this ran")
+
         self.point1 = Point(self.coordinates[0][0],self.coordinates[0][1],
                             self.coordinates[0][2])
         self.point2 = Point(self.coordinates[1][0],self.coordinates[1][1],
@@ -27,3 +30,46 @@ class Road:
 
         else:
             self.valid = False
+
+    def __eq__(self,other):
+        # print(self.coordinates,other.coordinates)
+        return(self.coordinates == other.coordinates)
+
+if __name__ == '__main__':
+    from point import Point
+    x = 17
+    y = 23
+    z = 24
+    while(True):
+        x2 = int(input("hex1: "))
+        y2 = int(input("hex2: "))
+        z2 = int(input("hex3: "))
+
+        x1 = int(input("hex1: "))
+        y1 = int(input("hex2: "))
+        z1 = int(input("hex3: "))
+        
+        p1 = Point(x1,y1,z1)
+        p2 = Point(x2,y2,z2)
+
+        
+        r1 = Road(p1,p2)
+        print("r1 initialized.\n")
+
+        x2 = int(input("hex1: "))
+        y2 = int(input("hex2: "))
+        z2 = int(input("hex3: "))
+
+        x1 = int(input("hex1: "))
+        y1 = int(input("hex2: "))
+        z1 = int(input("hex3: "))
+        
+        p3 = Point(x1,y1,z1)
+        p4 = Point(x2,y2,z2)
+
+        r2 = Road(p3,p4)
+
+        if(r1==r2):
+            print("same road")
+        else:
+            print("distinct roads")
