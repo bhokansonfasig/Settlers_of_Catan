e8d72d88cd2808af1dbf599fd24b63c339f784ea
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-02-27 18:46:02 -0600

    Made AI_code 1 play randomly.

--- catan_AI.py
+++ catan_AI.py
@@ -4,17 +4,34 @@ from road import Road
 
 def computer_choose_settlement(computer,players):
     """Has computer place settlement. Returns tuple of the placed settlement"""
-
-    return Point(0,1,7)
+    if computer.AI_code==1:
+        # Randomly place settlement
+        available_points = legal_settlement_placements(computer,players)
+        return choice(available_points)
+    else:
+        # Place settlement in the ocean
+        return Point(0,1,7)
 
 
 def computer_choose_road(computer,players):
     """Has computer place road. Returns tuples of the placed road"""
-
-    return Road(Point(0,1,7),Point(1,7,8))
+    if computer.AI_code==1:
+        # Randomly place road
+        available_roads = legal_road_placements(player,players)
+        return choice(available_roads)
+    else:
+        # Place road in the ocean
+        return Road(Point(0,1,7),Point(1,7,8))
 
 
 def computer_choose_city(computer,players):
     """Has computer place city. Returns tuple of the placed settlement"""
-
-    return Point(0,1,7)
+    if computer.AI_code==1:
+        # Randomly place city
+        available_points = []
+        for point in computer.settlements:
+            available_points.append(point)
+        return choice(available_points)
+    else:
+        # Place city in the ocean
+        return Point(0,1,7)
