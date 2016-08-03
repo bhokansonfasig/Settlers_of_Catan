87b977fb8442b3d08925eb61e20bf122f90aef37
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2016-08-03 14:35:39 -0500

    Updated README since longest road has been implemented

--- player.py
+++ player.py
@@ -48,12 +48,12 @@ class Player:
 		self.stone = 0
 
 		# For testing, add resources to human players and fewer to computers
-		if self.AI_code<0:
-			self.wood += 20
-			self.brick += 20
-			self.wheat += 20
-			self.sheep += 20
-			self.stone += 20
+		# if self.AI_code<0:
+		# 	self.wood += 20
+		# 	self.brick += 20
+		# 	self.wheat += 20
+		# 	self.sheep += 20
+		# 	self.stone += 20
 		# else:
 		# 	self.wood += 20
 		# 	self.brick += 20
@@ -62,7 +62,7 @@ class Player:
 
 		# Score keeping
 		self.score = 0
-		self.road_length = 0 
+		self.road_length = 0
 		self.has_longest_road = False
 		self.has_largest_army = False # For once we do development cards
 
@@ -123,5 +123,3 @@ class Player:
 			return True
 		else:
 			return False
-
-
